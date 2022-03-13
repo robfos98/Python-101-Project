6ddda06f25601fcb2641d07473dbf19316a11cb0
Robert <robfos98@gmail.com>
2022-03-12 21:08:02 -0500

    succinctification

--- rps.py
+++ rps.py
@@ -1,32 +1,21 @@
 import random
 rps = 'prs'
-p = 'Paper'
-r = 'Rock'
-s = 'Scissors'
-win = 0
-loss = 0
+p, r, s = 'Paper', 'Rock', 'Scissors'
+win = loss = 0
 intro = 'Please throw Rock, Paper, or Scissors (R, P, S), or throw Q to quit.'
 def throw():
     print('\nThrow:')
-    x = input()
-    x = x.lower()
-    if x == 'p' or x == p.lower():
-        return 'p'
-    elif x == 'q' or x == 'quit':
-        return 'q' 
-    elif x == 'r' or x == r.lower():
-        return 'r'
-    elif x == 's' or x == s.lower():
-        return 's'
+    x = input().lower()
+    if x == 'p' or x == p.lower(): return 'p'
+    elif x == 'q' or x == 'quit': return 'q' 
+    elif x == 'r' or x == r.lower(): return 'r'
+    elif x == 's' or x == s.lower(): return 's'
     else: return f'I\'m sorry, I didn\'t catch that. {intro}'
 def output(letter):
     match letter:
-        case 'p':
-            return p
-        case 'r':
-            return r
-        case 's':
-            return s
+        case 'p': return p
+        case 'r': return r
+        case 's': return s
 
 print('\nHello!')
 print('This program can play Rock, Paper, Scissors.')
@@ -37,12 +26,10 @@ while True:
         print(x)
         continue
     x = rps.find(x)
-    if x < 0:
-        break
+    if x < 0: break
     beat = random.choice(rps)
-    match x - rps.find(beat) % 3:
-        case 0:
-            print('A tie.')
+    match (x - rps.find(beat)) % 3:
+        case 0: print('A tie.')
         case 1:
             print(f'Sorry, but I threw {output(beat)}.')
             loss += 1;
