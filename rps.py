30a9a5739e295304b94cae0008de6d3adc74fc98
Robert <robfos98@gmail.com>
2022-03-12 19:04:13 -0500

    Project

---
+++ rps.py
@@ -0,0 +1,54 @@
+import random
+rps = 'prs'
+p = 'Paper'
+r = 'Rock'
+s = 'Scissors'
+win = 0
+loss = 0
+intro = 'Please throw Rock, Paper, or Scissors (R, P, S), or throw Q to quit.'
+def throw():
+    print('Throw:')
+    x = input()
+    x = x.lower()
+    if x == 'p' or x == p.lower():
+        return 'p'
+    elif x == 'q' or x == 'quit':
+        return 'q'
+    elif x == 'r' or x == r.lower():
+        return 'r'
+    elif x == 's' or x == s.lower():
+        return 's'
+    else: return f'I\'m sorry, I didn\'t catch that. {intro}'
+def output(letter):
+    match letter:
+        case 'p':
+            return p
+        case 'r':
+            return r
+        case 's':
+            return s
+
+print('\nHello!')
+print('This program can play Rock, Paper, Scissors.')
+print(intro)
+while True:
+    print('')
+    x = throw()
+    if len(x) > 1:
+        print(x)
+        continue
+    x = rps.find(x)
+    if x < 0:
+        break
+    beat = random.choice(rps)
+    x -= rps.find(beat)
+    match x % 3:
+        case 0:
+            print('A tie.')
+        case 1:
+            print(f'Sorry, but I threw {output(beat)}.')
+            loss += 1;
+        case 2:
+            print(f'Nice, you beat {output(beat)}.')
+            win += 1;
+    print(f'You\'ve won {win} games, and lost {loss}.')
